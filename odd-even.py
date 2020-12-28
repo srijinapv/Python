ec081f97be5b580c8adf2582aea4347bf77aef26
srijinapv <srijinapv@yahoo.com>
2020-12-28 11:13:55 +0530

    updating program to main function

--- odd-even.py
+++ odd-even.py
@@ -1,20 +1,17 @@
 #!/Users/srijina/opt/miniconda3/bin/python
 
-starting_number = int(input('Enter Starting range : '))
-ending_number = int(input('Enter Ending range : '))
-odd_numbers = []
-for value in range(starting_number, ending_number+1):
-    if value % 2 != 0:
-        
-        odd_numbers.append(value)
-print(odd_numbers)
-count_odd_number = len(odd_numbers)
-print(count_odd_number)
-largest_odd_number = odd_numbers[-1]
-print(largest_odd_number)
-
-def main():
-	
+def main(starting_number,ending_number):
+	odd_numbers = []
+	for value in range(starting_number, ending_number+1):
+	    if value % 2 != 0:
+	    	odd_numbers.append(value)
+	print(odd_numbers)
+	count_odd_number = len(odd_numbers)
+	print(count_odd_number)
+	largest_odd_number = odd_numbers[-1]
+	print(largest_odd_number)
 
 if __name__ == '__main__':
-	main()
+	starting_number = int(input('Enter Starting range : '))
+	ending_number = int(input('Enter Ending range : '))
+	main(starting_number,ending_number)
