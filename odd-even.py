3a60fc4bc307c39bd32becea7578b51456626fb9
srijinapv <srijinapv@yahoo.com>
2020-12-31 23:24:13 +0530

    indentation for odd-even

--- odd-even.py
+++ odd-even.py
@@ -3,13 +3,14 @@
 def main(starting_number,ending_number):
 	odd_numbers = []
 	for value in range(starting_number, ending_number+1):
-	    if value % 2 != 0:
-	    	odd_numbers.append(value)
+		if value % 2 != 0:
+			odd_numbers.append(value)
 	print(odd_numbers)
 	count_odd_number = len(odd_numbers)
 	print(count_odd_number)
 	largest_odd_number = odd_numbers[-1]
 	print(largest_odd_number)
+
 
 if __name__ == '__main__':
 	starting_number = int(input('Enter Starting range : '))
