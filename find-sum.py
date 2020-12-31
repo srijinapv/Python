afb2ff41d24a8cb96a7d918a531ab835c0c82779
srijinapv <srijinapv@yahoo.com>
2020-12-31 23:12:50 +0530

    correcting indentation

--- find-sum.py
+++ find-sum.py
@@ -1,7 +1,6 @@
-#!/Users/srijina/opt/miniconda3/bin/python
-
 class Calculate():
     list_to_store = []
+
     def __init__(self, number):
         self.number = number
         
@@ -11,11 +10,12 @@ class Calculate():
     def sumof():
         return sum(Calculate.list_to_store)
 
+
 if __name__ == '__main__':
-	times = int(input('How many numbers want to be added : '))
-	for _ in range(times):
-		value = int(input())
-		obj = Calculate(value)
-		obj.store()
-	print(Calculate.sumof())
+    times = int(input('How many numbers want to be added : '))
+    for _ in range(times):
+        value = int(input())
+        obj = Calculate(value)
+        obj.store()
+    print(Calculate.sumof())
     #print(sum(Calculate.list_to_store))
