21100eb970ece4dce532e54d40cb2c52a1b30cea
srijinapv <srijinapv@yahoo.com>
2021-01-15 22:37:59 +0530

    Finding area of shapes according to user choice

--- find-area.py
+++ find-area.py
@@ -1,32 +1,53 @@
+class Circle:
 
-class Area:
-    def __init__(self, length, base, radius,height):
-        self.length = length
-        self.base = base
+    def __init__(self, radius):
         self.radius = radius
+
+    def circle_area(self):
+        area_circle = 3.14 * self.radius * self.radius
+        return area_circle
+
+
+class Rectangle:
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def rectangle_area(self):
+        area_rectangle = self.length * self.width
+        return area_rectangle
+
+
+class Triangle:
+
+    def __init__(self, base, height):
+        self.base = base
         self.height = height
 
-    def area_rect(self):
-        area_of_rectangle = self.length * self.base
-        return area_of_rectangle
+    def triangle_area(self):
+        area_triangle = 0.5 * self.base * self.height
+        return area_triangle
 
-    def area_circle(self):
-        area_of_circle = 3.14 * self.radius * self.radius
-        return area_of_circle
-        
-    def area_triangle(self):
-        area_of_triangle = 0.5 * self.base * self.height
-        return area_of_triangle
 
 if __name__ == '__main__':
-    
-    length = int(input("Enter length :"))
-    base = int(input("Enter base : "))
-    radius = int(input("Enter radius :"))
-    height = int(input("Enter height :"))
 
-    obj = Area(length, base, radius,height)
+    user_choice = int(input("User Choice ( 1 for circle, 2 for rectangle , 3 for triangle):"))
 
-    print(obj.area_rect())
-    print(obj.area_circle())
-    print(obj.area_triangle())
+    if user_choice == 1:
+        radius = int(input("Enter radius :"))
+        obj_circle = Circle(radius)
+        print(f"area of circle is {obj_circle.circle_area()}")
+
+    elif user_choice == 2:
+        length = int(input("Enter length :"))
+        width = int(input("Enter width :"))
+        obj_rectangle = Rectangle(length, width)
+        print(f"area of rectangle is {obj_rectangle.rectangle_area()}")
+
+    elif user_choice == 3:
+        base = int(input("Enter base : "))
+        height = int(input("Enter height :"))
+        obj_triangle = Triangle(base, height)
+        print(f"area of triangle is {obj_triangle.triangle_area()}")
+
